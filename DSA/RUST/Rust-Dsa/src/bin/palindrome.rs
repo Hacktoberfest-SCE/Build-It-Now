8ec4d115e54db37224aa9105fcc658bc5a2f948f
Nagaprasad v r <nagaprasadvr246@gmail.com>
2022-10-09 20:47:27 +0530

    palindrome and linked list

--- DSA/RUST/Rust-Dsa/src/bin/palindrome.rs
+++ DSA/RUST/Rust-Dsa/src/bin/palindrome.rs
@@ -1,5 +1,7 @@
+#![feature(test)]
+extern crate test;
 fn main() {
-    match check_palindromw("malayalam") {
+    match check_palindrome("malayalam") {
         Ok(data) => {
             println!("{}", data);
         }
@@ -9,7 +11,7 @@ fn main() {
     }
 }
 
-fn check_palindromw(s: &str) -> Result<String, String> {
+fn check_palindrome(s: &str) -> Result<bool, String> {
     let v: Vec<char> = s.chars().collect();
     let mut i = 0;
     let mut j = v.len() - 1;
@@ -23,5 +25,27 @@ fn check_palindromw(s: &str) -> Result<String, String> {
         j -= 1;
     }
 
-    Ok(String::from("Palindrome String"))
+    Ok(true)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        assert_eq!(check_palindrome("malayalam"), Ok(true));
+    }
+    #[test]
+    fn test2() {
+        assert_eq!(
+            check_palindrome("helloworld"),
+            Err(String::from("Not a palindrome String!"))
+        );
+    }
+
+    #[bench]
+    fn time_complexity(b: &mut test::Bencher) {
+        b.iter(|| check_palindrome("abcdefghhgfedcba"));
+    }
 }
